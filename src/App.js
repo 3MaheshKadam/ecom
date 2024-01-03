e7b694781dc0b5adfb64d2d6d0050e259de6b1b9
3MaheshKadam <86357696+3MaheshKadam@users.noreply.github.com>
2024-01-03 22:40:21 +0530

    commit-1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./Components/Assets/Navbar/Navbar";
 
 function App() {
-  return <h1>hello first day of e commerceh1</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
